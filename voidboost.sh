8bdd413a3b4ec9b1a97e44a2c84df0083fee1de3
GokyB <58221166+GokyB@users.noreply.github.com>
2022-01-26 00:11:47 +0300

    Minor bug fixes

--- voidboost.sh
+++ voidboost.sh
@@ -12,7 +12,7 @@ sudo ln -S /etc/sv/dbus /var/service
 sleep 3
 
 echo "Installing Pulseaudio."
-sudo xbps-install -S pulseaudio alsa-plugins-pulseaudio
+sudo xbps-install -Sy pulseaudio alsa-plugins-pulseaudio
 sudo ln -S /etc/sv/pulseaudio /var/service
 
 sleep 3
@@ -20,13 +20,13 @@ sleep 3
 read -p  "Please select a desktop enviroment (gnome/kde/xfce): " DE
 case $DE in
     [gGnNoOmMeE])
-        sudo xbps-install -S gnome gnome-apps gdm && sudo ln -S /etc/sv/gdm /var/service/
+        sudo xbps-install -Sy gnome gnome-apps gdm && sudo ln -S /etc/sv/gdm /var/service/
         ;;
     [kKdDeE])
-        sudo xbps install -S kde5 plasma-desktop sddm && sudo ln -S /etc/sv/sddm /var/service/
+        sudo xbps install -Sy kde5 plasma-desktop sddm && sudo ln -S /etc/sv/sddm /var/service/
         ;;
     [xXfFcCeE])
-        sudo xbps-install -S xfce4 xfce4-screensaver xfce4-session xfce4-terminal lightdm-gtk-greeter && sudo ln -S /etc/sv/lightdm var/service/
+        sudo xbps-install -Sy xfce4 xfce4-screensaver xfce4-session xfce4-terminal lightdm-gtk-greeter && sudo ln -S /etc/sv/lightdm var/service/
         ;;
     *)
         echo "Please enter a correct input."
@@ -42,13 +42,13 @@ echo ""
 echo "Please select the graphic card that you are using (intel/amd/nvidia): " GPU
 case $GPU in 
     [iIİnNtTeElL])
-        sudo xbps-install -S vulkan-loader mesa-vulkan-intel intel-media-driver intel-video-accel
+        sudo xbps-install -Sy vulkan-loader mesa-vulkan-intel intel-media-driver intel-video-accel
         ;;
     [aAmMdD])
-        sudo xbps-install -S vulkan-loader mesa-vulkan mesa-vaapi mesa-vdpau
+        sudo xbps-install -Sy vulkan-loader mesa-vulkan mesa-vaapi mesa-vdpau
         ;;
     [nNvViIİddiIİa])
-        sudo xbps-install -S nvidia nvidia-libs-32bit nvidia-settings
+        sudo xbps-install -Sy nvidia nvidia-libs-32bit nvidia-settings
         ;;
     *)
         echo "Please enter a valid driver."
